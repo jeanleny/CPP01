19e5ecc0eacd3fcfeb70c95d91b23005da592fdc
Leny Peris <lperis@3B4.42angouleme.fr>
2025-11-07 11:33:51 +0100

    git setup

---
+++ ex00/include/Zombie.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+class Zombie
+{
+	public :
+		Zombie();
+		~Zombie();
+	private :
+};
+
+
+#endif
---
+++ ex00/src/Zombie.cpp
@@ -0,0 +1,7 @@
+#include <Zombie.hpp>
+
+Zombie::Zombie(){
+	std::cout<<"ntm"<<std::endl;
+	return ;}
+
+Zombie::~Zombie(){return ;}
